e733677e4efb887c0335db9bff671f5dd1aae5b7
Sergio <gonzalessergio3144@gmail.com>
2024-03-28 14:31:51 -0500

    news components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-function App() {
+import { Guitar } from "./components/Guitar"
+import { Header } from "./components/Header"
 
+function App() {
+ 
   return (
-    <>
-      <h1>Perifericos PC</h1>
+    <> 
+    <Header />
+    <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          <Guitar />
+        </div>
+    </main>
+
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+    </footer>
     </>
   )
 }
